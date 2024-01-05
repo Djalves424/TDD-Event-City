725f8042c2f7c59a2dd8d44a7c09777bdb022b04
djalves424 <djalves424@gmail.com>
2024-01-04 23:31:47 -0300

    Created classes CityService and CityController

---
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -0,0 +1,27 @@
+package com.devsuperior.demo.services;
+
+import com.devsuperior.demo.dto.CityDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CityService {
+
+    @Autowired
+    CityRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll() {
+        List<City> list = repository.findAll(Sort.by("name"));
+        return list.stream()
+                .map(x -> new CityDTO(x))
+                .collect(Collectors.toList());
+    }
+}
